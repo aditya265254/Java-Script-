845a57a1e7289bcd8efc557c617e06c91dd07240
Aditya singh <144699096+aditya265254@users.noreply.github.com>
2026-03-28 17:11:01 +0000

    commit

--- 05_ittreation/one.js
+++ 05_ittreation/one.js
@@ -1,6 +1,10 @@
 //for
 
-for (let index = 0; index <= 10; index++) {
-  const element = index;
-  console.log(element);
+for (let i = 0; i <= 10; i++) {
+  const element = i;
+  if (element == 5) {
+    console.log("5 is best no ");
+    console.log(element);
+
+  }
 }
